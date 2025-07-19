7c3dd1ef18ad1e17f029f3abfc3f37e13385bc9b
Andy Estrella Ruiz <skull.penta5@gmail.com>
2025-07-19 02:38:52 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <OrderCard order={

--- src/app/(app)/kds/page.tsx
+++ src/app/(app)/kds/page.tsx
@@ -57,7 +57,7 @@ export default function KdsPage() {
         )
     }
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 gap-2 py-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 gap-2 py-4 items-start">
             {orderList.map(order => (
                 <OrderCard key={order.id} order={order} onUpdateItemStatus={updateItemStatus} />
             ))}
