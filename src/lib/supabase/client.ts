d2332f16047e19b00501c4d5ba1ac3ad8c0ace67
Andy Estrella Ruiz <skull.penta5@gmail.com>
2025-07-26 15:43:20 +0000

    dont hardcode the solution i think this is what its making it go wrong.

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -3,7 +3,7 @@ import { createBrowserClient } from '@supabase/ssr'
 export function createClient() {
   // Create a supabase client on the browser with project's credentials
   return createBrowserClient(
-    "https://eteohwcqyobdeyxyufcva.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0ZW9od2NxeW9iZGV5eXVmY3ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0ODI1MzcsImV4cCI6MjA2OTA1ODUzN30.vIz-fwKmuoRwFHi2d1w_XOYfxDDbDNH3T3jMGcblr5w"
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
