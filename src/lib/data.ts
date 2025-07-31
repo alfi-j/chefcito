c99d6a3c4b67fb419563331ada9f22999c337507
Andy Estrella Ruiz <skull.penta5@gmail.com>
2025-07-31 18:07:19 +0000

    delete all leftovers on the code like unused files

--- src/lib/data.ts
+++
@@ -1,6 +0,0 @@
-
-// This file is DEPRECATED and will be removed.
-// All data fetching is now handled by API routes,
-// which in a real app would connect to a database.
-// For local development, mock data is temporarily
-// defined within these API routes.
