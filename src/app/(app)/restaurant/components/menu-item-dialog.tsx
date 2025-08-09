4b80bc601fcb0352a5e1c5c5ec345e4cff1385ff
Andy Estrella Ruiz <skull.penta5@gmail.com>
2025-08-09 00:37:59 +0000

    analyze and fix

--- src/app/(app)/restaurant/components/menu-item-dialog.tsx
+++ src/app/(app)/restaurant/components/menu-item-dialog.tsx
@@ -142,7 +142,7 @@ export function MenuItemDialog({
           </DialogDescription>
         </DialogHeader>
         
-        <div className="flex-1 min-h-0">
+        <div className="flex-1 relative min-h-0">
           <ScrollArea className="h-full">
             <div className="space-y-4 py-4 pr-6">
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
